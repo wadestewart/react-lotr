2aaf2374beb11af69364e767f98b93e0091cbf63
Wade Stewart <stew.wade@gmail.com>
2018-07-05 15:42:25 -0400

    rendering Movie component

---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Movie extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Lord of the Rings: A Trilogy</h1>
+                <p>4h 37min</p>
+            </div>
+        )
+    }
+}
+
+export default Movie
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Movie from './Movie'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Movie title="The Fellowship of the Ring"/>,
+    document.getElementById('root'))
+registerServiceWorker()
